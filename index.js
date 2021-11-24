2223fd02686ae277fd722294679a8d4222cda572
WilliamDenega <williamdenega@gmail.com>
2021-11-24 13:46:14 -0500

    commit tree 4be88ac31e19970ac01d66d6ad82caf55e1aecd2

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+function findMatching(drivers,name){
+
+    return drivers.filter((names)=>names.toLowerCase() === name.toLowerCase());
+
+
+};
+
+function fuzzyMatch(drivers,letters){
+    return drivers.filter((names)=> names.startsWith(letters));
+};
+
+
+function matchName(drivers,searchedName){
+  
+    return drivers.filter(person => person.name === searchedName)
+};
